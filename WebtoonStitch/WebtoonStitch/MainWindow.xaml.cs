3d75a7eca7cc2c2915c71d5b3cc5b0315460ac05
Yun Zhi Lin <yunzhi246@gmail.com>
2018-06-13 00:14:32 -0400

    Allows png. Added resize functionality. Changed how switching between views worked

--- WebtoonStitch/WebtoonStitch/MainWindow.xaml.cs
+++ WebtoonStitch/WebtoonStitch/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -27,41 +29,49 @@ namespace WebtoonStitch
 
         private SortedList<string, string> masterFileNames;
         private Bitmap finalImage;
+        private String view = "START";
 
         public MainWindow()
         {
             InitializeComponent();
-            resetStart();
+            updateView();
         }
 
         public string Directory { get; private set; }
 
-        private void StartWorking()
+        private bool StartWorking()
         {
             List<string> fileNames = new List<string>();
             this.Directory = ChooseFolder();
 
-            if (this.Directory != string.Empty)
+            if (this.Directory == string.Empty)
             {
-                string[] files = System.IO.Directory.GetFiles(this.Directory, "*.jpg");
-                fileNames = files.ToList<string>();
+               // System.Windows.MessageBox.Show("Error please try again!");
+                return false;
             }
 
+            //FIX THIS LATER, SKETCHY WAY
+            string[] files = System.IO.Directory.GetFiles(this.Directory, "*.jpg");
+            fileNames = files.ToList<string>();
             masterFileNames = new SortedList<string, string>();
             foreach (string fileName in fileNames)
             {
                 int lastIndex = fileName.LastIndexOf(@"\");
                 masterFileNames.Add(fileName.Substring(lastIndex + 1), fileName);
             }
+            files = System.IO.Directory.GetFiles(this.Directory, "*.png");
+            fileNames = files.ToList<string>();
+            foreach (string fileName in fileNames)
+            {
+                int lastIndex = fileName.LastIndexOf(@"\");
+                masterFileNames.Add(fileName.Substring(lastIndex + 1), fileName);
+            }
 
-            this.FilesListBox.ItemsSource = masterFileNames.Keys.ToList();
+            List<String> sortedNames = masterFileNames.Keys.ToList();
+            sortedNames.Sort();
+            this.FilesListBox.ItemsSource = sortedNames;
 
-            //visibilities
-            this.StartButton.Visibility = Visibility.Hidden;
-            this.FilesListBox.Visibility = Visibility.Visible;
-            this.StitchButton.Visibility = Visibility.Visible;
-            this.CancelButton.Visibility = Visibility.Visible;
-
+            return true;
         }
         private string ChooseFolder()
         {
@@ -69,7 +79,7 @@ namespace WebtoonStitch
             FolderBrowserDialog chooseFolder = new FolderBrowserDialog();
             chooseFolder.ShowNewFolderButton = false;
             chooseFolder.RootFolder = Environment.SpecialFolder.MyComputer;
-            chooseFolder.SelectedPath = @"Z:\new";
+            chooseFolder.SelectedPath = ""/*@"Z:\new"*/;
             DialogResult result = chooseFolder.ShowDialog();
             if (result == System.Windows.Forms.DialogResult.OK)
             {
@@ -79,10 +89,11 @@ namespace WebtoonStitch
             return path;
         }
 
-        private int StitchImages()
+        private void StitchImages()
         {
             List<string> keys = (List<string>)FilesListBox.ItemsSource;
             List<System.Drawing.Image> images = new List<System.Drawing.Image>();
+            List<System.Drawing.Image> resizedImages = new List<System.Drawing.Image>();
             int totalHeight = 0;
             int finalWidth = 0;
             foreach(string key in keys)
@@ -93,84 +104,151 @@ namespace WebtoonStitch
                 {
                     System.Drawing.Image img = System.Drawing.Image.FromFile(filename);
                     images.Add(img);
-                    totalHeight += img.Height;
                     if (img.Width > finalWidth) { finalWidth = img.Width; }
                 }
             }
+            if ((bool)CustomWidthCheckBox.IsChecked)
+            {
+                int temp;
+                bool isInt = int.TryParse(CustomWidthTextBox.Text, out temp);
+                if (isInt)
+                {
+                    finalWidth = temp;
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Width entered not an integer, will default to largest width.");
+                }
+            }
+
+            foreach (System.Drawing.Image img in images)
+            {
+                System.Drawing.Image rImg = ResizeImage(img, finalWidth);
+                totalHeight += rImg.Height;
+                resizedImages.Add(rImg);
+            }
+            
             Bitmap bitmap = new Bitmap(finalWidth, totalHeight);
             using (Graphics g = Graphics.FromImage(bitmap))
             {
                 int yPos = 0;
-                foreach(System.Drawing.Image img in images)
+                foreach(System.Drawing.Image img in resizedImages)
                 {
                     g.DrawImage(img, 0, yPos);
                     yPos += img.Height;
                 }
             }
             finalImage = bitmap;
-            return totalHeight;
-         //   bitmap.Save(this.Directory+"\\FINALIMAGE.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
 
+        public static System.Drawing.Image ResizeImage(System.Drawing.Image image, int width)
+        {
+            float fhei = ((float)image.Height * ((float)width / (float)image.Width));
+            int height = (int)fhei;
+            if (image.Height == height && image.Width == width) return image;
 
-            /*
-            Bitmap bitmap = new Bitmap(image1.Width + image2.Width, Math.Max(image1.Height, image2.Height));
-            using (Graphics g = Graphics.FromImage(bitmap))
+            var destRect = new System.Drawing.Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+    //        destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
             {
-                g.DrawImage(image1, 0, 0);
-                g.DrawImage(image2, image1.Width, 0);
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
             }
-            */
+
+            return destImage;
+        }
+
+        private void displayPreview()
+        {
+            System.Drawing.Image displayImage = ResizeImage(finalImage, 700);
+            MemoryStream ms = new MemoryStream();
+            displayImage.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            BitmapImage bImg = new BitmapImage();
+            bImg.BeginInit();
+            bImg.StreamSource = new MemoryStream(ms.ToArray());
+            bImg.EndInit();
+            PreviewImage.Source = bImg;
+            PreviewImage.Height = displayImage.Height;
+      //      PreviewImage.Width = finalImage.Width - 50;
+        }
+
+        private void updateView()
+        {
+            StartGrid.Visibility = Visibility.Hidden;
+            ShowFilesGrid.Visibility = Visibility.Hidden;
+            PreviewGrid.Visibility = Visibility.Hidden;
+
+            if (view == "START")
+            {
+                StartGrid.Visibility = Visibility.Visible;
+            }
+            else if (view == "FILES")
+            {
+                ShowFilesGrid.Visibility = Visibility.Visible;
+            }
+            else if (view == "PREVIEW")
+            {
+                PreviewGrid.Visibility = Visibility.Visible;
+            }
         }
 
 
         //*********BUTTON CLICKS*********
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            this.StartWorking();
+            if (this.StartWorking())
+            {
+                view = "FILES";
+                updateView();
+            }
         }
         private void StitchButton_Click(object sender, RoutedEventArgs e)
         {
-            int height = StitchImages();
-
-            MemoryStream ms = new MemoryStream();
-            finalImage.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            BitmapImage bImg = new BitmapImage();
-            bImg.BeginInit();
-            bImg.StreamSource = new MemoryStream(ms.ToArray());
-            bImg.EndInit();
-            PreviewImage.Source = bImg;
-            PreviewImage.Height = height;
-            
-            FilesListBox.Visibility = Visibility.Hidden;
-            StitchButton.Visibility = Visibility.Hidden;
-            CancelButton.Visibility = Visibility.Hidden;
-            PreviewViewbox.Visibility = Visibility.Visible;
-            PreviewScrollViewer.Visibility = Visibility.Visible;
-            SaveButton.Visibility = Visibility.Visible;
-            PreviewImage.Visibility = Visibility.Visible;
+            StitchImages();
+            displayPreview();
+            view = "PREVIEW";
+            updateView();
         }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        private void FilesCancelButton_Click(object sender, RoutedEventArgs e)
         {
-            resetStart();
+            view = "START";
+            updateView();
         }
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             finalImage.Save(this.Directory + "\\FINALIMAGE.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-            resetStart();
+            view = "START";
+            updateView();
+        }
+        private void PreviewCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            view = "START";
+            updateView();
         }
 
-        private void resetStart()
+        private void CustomWidthCheckBox_Click(object sender, RoutedEventArgs e)
         {
-            this.StartButton.Visibility = Visibility.Visible;
-            this.FilesListBox.Visibility = Visibility.Hidden;
-            this.StitchButton.Visibility = Visibility.Hidden;
-            this.CancelButton.Visibility = Visibility.Hidden;
-            PreviewViewbox.Visibility = Visibility.Hidden;
-            PreviewScrollViewer.Visibility = Visibility.Hidden;
-            SaveButton.Visibility = Visibility.Hidden;
-            PreviewImage.Visibility = Visibility.Hidden;
+            if ((bool)CustomWidthCheckBox.IsChecked)
+            {
+                CustomWidthTextBox.IsEnabled = true;
+            }
+            else
+            {
+                CustomWidthTextBox.IsEnabled = false;
+            }
         }
-        
     }
 }
